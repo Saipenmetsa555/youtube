722ac978600fc34d8a901fef60911e9bb6808a10
Saipenmetsa555 <89723740+Saipenmetsa555@users.noreply.github.com>
2024-04-18 09:07:44 +0530

    bug resolved

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { closeMenu } from "./utils/appSlice";
 import { useSearchParams } from "react-router-dom";
 
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
+  const mobileOpen = useSelector((store) => store.app.mobileOpen);
 
   const dispatch = useDispatch();
   useEffect(() => {
@@ -12,19 +13,23 @@ const WatchPage = () => {
   });
 
   return (
-    <div className="w-full md:w-screen">
-      <div className="relative" style={{ paddingTop: "56.25%" }}>
-        <iframe
-          className="absolute inset-0 w-full h-4/5 md:w-3/5 md:h-2/4 md:m-5"
-          src={"https://www.youtube.com/embed/" + searchParams.get("v")}
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerpolicy="strict-origin-when-cross-origin"
-          allowfullscreen
-        ></iframe>
-      </div>
-    </div>
+    <>
+      {!mobileOpen && (
+        <div className="w-full md:w-screen">
+          <div className="relative" style={{ paddingTop: "56.25%" }}>
+            <iframe
+              className="absolute inset-0 w-full h-4/5 md:w-3/5 md:h-2/4 md:m-5"
+              src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
